2cee6c05fa5fe37cd63064abd35a5c6ef2075902
Adicus Alexander <92056056+Adicus-Alexander@users.noreply.github.com>
2022-10-04 18:48:59 -0500

    Add files via upload

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NCalc;
+
+
+public class Calculator : MonoBehaviour
+{
+   public string Calculate(string equation)
+   {
+    Expression expression = new Expression(equation);
+    return expression.Evaluate().ToString();
+   }
+
+}
